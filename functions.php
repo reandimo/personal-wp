71da14205140d4ad4241128cd285123042416e77
Renan Diaz <reandimo23@gmail.com>
2026-03-13 16:20:31 -0300

    Fixes

--- functions.php
+++ functions.php
@@ -63,3 +63,33 @@ if (file_exists($acf_theme_options)) {
 if (class_exists('PersonalTheme\Theme\ThemeSetup')) {
     new PersonalTheme\Theme\ThemeSetup();
 }
+
+/**
+ * Remove WordPress block styles completely.
+ */
+add_action('wp_enqueue_scripts', function() {
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-blocks-style');
+    wp_dequeue_style('classic-theme-styles');
+    wp_dequeue_style('global-styles');
+}, 100);
+
+/**
+ * Remove block styles from admin/editor as well.
+ */
+add_action('admin_enqueue_scripts', function() {
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+});
+
+/**
+ * Disable WordPress global styles (theme.json output).
+ */
+add_filter('wp_theme_json_get_style_nodes', '__return_empty_array');
+
+/**
+ * Remove global styles inline CSS completely.
+ */
+remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
+remove_action('wp_footer', 'wp_enqueue_global_styles', 1);
